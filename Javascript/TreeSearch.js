59855e5d713559d0f50dfcaff7d5268ebf5f6e5b
Freddie Rawlins <freddie.rawlins@mail.com>
2020-04-16 12:34:07 +0100

    Update TreeSearch.js

--- Javascript/TreeSearch.js
+++ Javascript/TreeSearch.js
@@ -10,14 +10,13 @@ function treeSearch() {
         mod = item in nodes[startID].attributes ? nodes[startID].attributes[item] : 0
         costs[item] = {1: (4-mod), 2: 2, 3: 2, 4: 2};
     }
-    // STILL NEED TO FIND OUT IF THIS IS FROM ORIGIN OR FROM OTHER NODES
-    // let results = solve(connections, startID)
     let results = connections
     for (let [key, value] of Object.entries(nodes)) {
-        if (["Skill", "Attribute"].includes(value.type)) {
+        if (value.name in costs) {
             let name = value.name
             //let cost = results[key].dist
             let cost = min(Object.values(results[key]))
+
             if (cost in costs[name] && costs[name][cost] > 0) {
                 costs[name][cost] = costs[name][cost] - 1
             } else {
@@ -31,39 +30,17 @@ function treeSearch() {
     }
 }
 
-/*
-function treeSearch(distance, nodes) {
-    let poss = [];
-    let visited = [];
-    let current = [];
-    
-    for(let [key, value] of Object.entries(nodes[startID])) {
-
-        if(value == distance) {
-            
-        }
-    }
-
-    return [poss, visited];
-}
-// */
-
-/*
-function randomPath(distance, visited) {
-    if (distance == 0){return visited}
-    let leaves = JSON.parse(JSON.stringify(visited))
-    let foundNext = false;
-    while (!foundNext && leaves.length != 0) {
-        let from = random(leaves)
-        let nodeID = -1
-        let fromLeaves = JSON.parse(JSON.stringify(Object.keys(connections[from])))
-        while (fromLeaves.length != 0) {
-            nodeID = random(fromLeaves)
-            // STILL NEED TO CHECK DISTANCE
-            if (!(nodeID in connections[from])) {
-
-            }
+function getXinY(number, stat, cons = connections) {
+    let result = solve(cons, startID)
+    let costs = {}
+    let routes = {}
+    for(let [key, value] of Object.entries(nodes)) {
+        if (value.name == stat) {
+            let temp = result[key]
+            let cost = temp.dist
+            let route = [from].concat(temp)
+            costs[cost] = key
+            routes[key] = route
         }
     }
 }
-// */
